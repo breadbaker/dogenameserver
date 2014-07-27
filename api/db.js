aa203a37935f8a7882eb968b06b4220d200edd2a
Dan Baker <dbaker@forio.com>
2014-07-27 10:50:10 -0700

    mongouri

--- api/db.js
+++ api/db.js
@@ -1,6 +1,19 @@
 // getting-started.js
 mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/test';
+
+
+mongoose.connect(mongoUri);
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
+});
 
 var NameSchema = mongoose.Schema({
     name: String,
