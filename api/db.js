7a8d1cb1eb834d4934c69684640012a3a54afb67
Dan Baker <dbaker@forio.com>
2014-07-27 11:02:31 -0700

    set heroku config:set NODE_ENV=production now remove mongo

--- api/db.js
+++ api/db.js
@@ -11,12 +11,12 @@ console.log(process.env.MONGOLAB_URI);
 
 mongoose.connect(mongoUri);
 
-mongo.Db.connect(mongoUri, function (err, db) {
-  db.collection('mydocs', function(er, collection) {
-    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
-    });
-  });
-});
+// mongo.Db.connect(mongoUri, function (err, db) {
+//   db.collection('mydocs', function(er, collection) {
+//     collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+//     });
+//   });
+// });
 
 var NameSchema = mongoose.Schema({
     name: String,
